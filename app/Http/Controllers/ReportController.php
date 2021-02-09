a444738e08973b6bc6d024ba76191e2bda52d52e
javohir01 <ruzikulovjavokhir005@gmail.com>
2021-02-09 20:27:08 +0500

    report

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -7,6 +7,10 @@ use Illuminate\Http\Request;
 
 class ReportController extends Controller
 {
+    $name = "sdfdsfgdf";
+    public function report() {
+        
+    }
     public function index()
     {
         $raw1 = 'count(edu_centers.id) as edu_center_count';
